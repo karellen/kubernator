dcb7af84f92643e8794a0cfbbacf62a6a8ba53c2
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-09-02 21:46:33 -0400

    Add gevent monkeypatching stanza

--- src/unittest/python/app_tests.py
+++ src/unittest/python/app_tests.py
@@ -15,6 +15,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+
+from gevent.monkey import patch_all, is_anything_patched
+
+if not is_anything_patched():
+    patch_all()
+
 import itertools
 import os
 import unittest
