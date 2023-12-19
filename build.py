2ee91dcb614a9e5a7d6908201d420425ffcc19c0
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-12-19 09:05:01 -0500

    Prepare 1.0.7 [skip ci]

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ use_plugin("python.vendorize")
 use_plugin("filter_resources")
 
 name = "kubernator"
-version = "1.0.6"
+version = "1.0.7.dev"
 
 summary = "Kubernator is the a pluggable framework for K8S provisioning"
 authors = [Author("Express Systems USA, Inc.", "")]
