8e06f110da688fe1d7e691fe2481c67be2e36aac
Arcadiy Ivanov <arcadiy@ivanov.biz>
2024-09-13 19:01:25 -0400

    Release v1.0.17

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ use_plugin("python.vendorize")
 use_plugin("filter_resources")
 
 name = "kubernator"
-version = "1.0.17.dev"
+version = "1.0.17"
 
 summary = "Kubernator is the a pluggable framework for K8S provisioning"
 authors = [Author("Express Systems USA, Inc.", "")]
